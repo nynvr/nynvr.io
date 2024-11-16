9d7063642f4d3cd9f522d5faf459d6398db316de
Noy Novera <noynovera@gmail.com>
2024-11-16 08:14:04 +0800

    empty amplify backend for now;

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,3 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
 
-defineBackend({
-  auth,
-  data,
-});
+defineBackend({});
