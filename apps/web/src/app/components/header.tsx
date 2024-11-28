041daeb6ed14d584a8fd5c2431c1191a79b3458f
Noy Novera <noynovera@gmail.com>
2024-11-28 11:39:49 +0800

    fix vertical alignment of darkmode toggle;

--- apps/web/src/app/components/header.tsx
+++ apps/web/src/app/components/header.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from "@/app/components/modeToggle";
 export default function Header() {
     return (
         <header
-            className="max-w-7xl mx-auto p-2 flex justify-between">
+            className="max-w-7xl mx-auto p-2 flex items-center justify-between">
             <Link href="/"
             className='p-2 border-2 text-2xl border-neutral-900 dark:border-neutral-100 font-black'>
                 NYNVR
