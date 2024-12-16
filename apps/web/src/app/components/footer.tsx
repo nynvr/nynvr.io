5c1c7168cfb5d4952e80dda56c1e6d16ac2c4d1b
Noy Novera <noynovera@gmail.com>
2024-12-17 07:35:46 +0800

    update footer message;

--- apps/web/src/app/components/footer.tsx
+++ apps/web/src/app/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
           <span className="leading-4 align-middle">&nbsp;2024 NYNVR</span>
           <span className="leading-4 align-middle">&nbsp;&bull;&nbsp;</span>
           <span className="text-xs leading-4 align-middle">
-            Life is so beautiful... but it is also full of pain and suffering. Let&apos;s try to be kind to one another.
+            Life is a beautiful journey yet it also has moments of challenges and suffering. In the face of it all, let us choose kindness.
           </span>
         </div>
       </footer>
